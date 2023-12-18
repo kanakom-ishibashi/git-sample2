9ae5a6b22285cb0008b0a36a2c7ae9d3fd90adf2
kanakom-ishibashi <kanakom001@gmail.com>
2023-12-18 06:36:16 +0000

    kadai07.js

---
+++ kadai07.js
@@ -0,0 +1,15 @@
+let fruits = {apple:'りんご', strawberry:'いちご', grape:'ぶどう', lemon:'レモン'};
+
+console.log(fruits['grape']);　
+
+let keys = Object.keys(fruits);  // キーの一覧を配列として取得
+
+console.log(fruits);
+
+
+
+var fruits = {apple:"りんご", strawberry:"いちご", grape:"ぶどう", lemon:"レモン"};
+
+for (key in fruits) {
+    console.log(fruits [key]);
+}

329032216c0494951f228708900be90007006883
kanakom-ishibashi <kanakom001@gmail.com>
2023-12-18 07:19:48 +0000

    kadai07.js

--- kadai07.js
+++ kadai07.js
@@ -7,9 +7,6 @@ let keys = Object.keys(fruits);  // キーの一覧を配列として取得
 console.log(fruits);
 
 
-
-var fruits = {apple:"りんご", strawberry:"いちご", grape:"ぶどう", lemon:"レモン"};
-
 for (key in fruits) {
     console.log(fruits [key]);
 }
